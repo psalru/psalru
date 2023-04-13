52588cd71cae0b642c601cf7d7b1377a50be1418
Alexey Safonov <alexey@safonov.pro>
2023-04-13 11:11:31 +0300

    state: удален предыдущий вариант сайта

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './style.scss'
-
-document.addEventListener('DOMContentLoaded', function () {
-    const  year = this.getElementById('year')
-
-    year.innerHTML = new Date().getFullYear();
-})
