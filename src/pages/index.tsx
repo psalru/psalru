b088556b0f38b7b3f3e7b7e33b27255009a2b282
Alexey Safonov <alexey@safonov.pro>
2023-08-03 17:20:36 +0300

    refactor: скорректированы title страниц

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title">Аналитика высшего образования</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
@@ -30,7 +30,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`ПСАЛ | ${siteConfig.title}`}
+      title='Аналитика высшего образования'
       description="Анализ высшего образования и университетов, на базе вузов — участников программ «Приоритет 2030» и «Передовые инженерные школы»">
       <HomepageHeader />
       <main>
